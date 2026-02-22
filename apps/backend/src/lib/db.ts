0a79797228d93155bd64c9e7e44cbcdaca6c0739
igmainc <igmainc@icloud.com>
2026-02-22 18:17:18 +0800

    chore: 删除无用log

--- apps/backend/src/lib/db.ts
+++ apps/backend/src/lib/db.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "@package/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = `${process.env.DATABASE_URL}`;
-console.log("[backend] connecting to database with connection string:", connectionString);
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
