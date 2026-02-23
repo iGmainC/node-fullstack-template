876c38b35a75121279df798d2cbbf01a55531cf6
igmainc <igmainc@icloud.com>
2026-02-23 18:17:26 +0800

    chore: 尝试把vite.config移出来

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import honoDevProxyPlugin from "../../package/vite-plugin-hono-dev";
+import honoDevProxyPlugin from "./package/vite-plugin-hono-dev";
 
 export default defineConfig({
   build: {
